ae29e85a48c5d168bc50f208bd9dd61f51a1cb63
ringo_sanchez <ringo196@gmail.com>
2019-04-04 13:44:51 -0700

    Implement fully working frontend.

--- dist/database/index.js
+++ dist/database/index.js
@@ -11,8 +11,6 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_mongoose2.default.Promise = Promise;
-
 var Schema = _mongoose2.default.Schema;
 
 var connection = _mongoose2.default.connect("mongodb://localhost/BandersGuru", {
--- dist/server.js
+++ dist/server.js
@@ -18,6 +18,16 @@ var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
 var _index = require("./database/index.js");
 
+var _mongoose = require("mongoose");
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _bluebird = require("bluebird");
+
+var Promise = _interopRequireWildcard(_bluebird);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -52,21 +62,26 @@ app.post("/game", function (req, res) {
 });
 
 app.get("/game/:id", function (req, res) {
-  _index.Game.findById({ _id: req.params.id }).then(function (game) {
-    _index.Scenario.find({ title: game.scenario }).then(function (scenario) {
-      var gameSave = {
-        id: game._id,
-        scenario: game.scenario,
-        currentStep: game.currentStep,
-        story: scenario[0].nodes.get(game.currentStep).story,
-        choices: scenario[0].nodes.get(game.currentStep).choices
-      };
-      res.json(gameSave);
-    }).catch(function (err) {
-      console.log(err);
-      res.send(err);
+  if (_mongoose2.default.Types.ObjectId.isValid(req.params.id)) {
+    _index.Game.findById({ _id: req.params.id }).then(function (game) {
+      _index.Scenario.find({ title: game.scenario }).then(function (scenario) {
+        var gameSave = {
+          id: game._id,
+          scenario: game.scenario,
+          currentStep: game.currentStep,
+          story: scenario[0].nodes.get(game.currentStep).story,
+          choices: scenario[0].nodes.get(game.currentStep).choices
+        };
+        res.json(gameSave);
+      }).catch(function (err) {
+        console.log(err);
+        throw err;
+        res.send(err);
+      });
     });
-  });
+  } else {
+    res.status(500).send();
+  }
 });
 
 app.post("/game/:id", function (req, res) {
@@ -77,18 +92,15 @@ app.post("/game/:id", function (req, res) {
     var newStep = {
       currentStep: choice.goto
     };
-    _index.Game.findOneAndUpdate({ _id: req.params.id }, newStep, { new: false }).then(function (game) {
+    _index.Game.updateOne({ _id: req.params.id }, newStep, { new: true }).then(function (game) {
       var gameSave = {
         id: req.params.id,
         scenario: req.body.scenario,
-        currentStep: game.currentStep,
-        story: scenario[0].nodes.get(game.currentStep).story,
-        choices: scenario[0].nodes.get(game.currentStep).choices
+        currentStep: newStep.currentStep,
+        story: scenario[0].nodes.get(newStep.currentStep).story,
+        choices: scenario[0].nodes.get(newStep.currentStep).choices
       };
       res.send(gameSave);
-    }).catch(function (err) {
-      console.log(err);
-      res.send(err);
     });
   });
 });
